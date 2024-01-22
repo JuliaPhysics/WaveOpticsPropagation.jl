38077d31987b1e69b068d79e4829bb0e4c096cbd
roflmaostc <fxw+git@mailbox.org>
2024-01-22 21:17:46 +0100

    Add test for volume AS

--- test/angular_spectrum.jl
+++ test/angular_spectrum.jl
@@ -21,20 +21,28 @@
 
         field = zeros(ComplexF64, (15, 15))
         field[5:6, 3:8] .= 1
-
         gg(x) = sum(abs2.(x .- angular_spectrum(cis.(x), 100e-6, 633e-9, 100e-6)[1]))
-
-        out2 = FiniteDifferences.grad(central_fdm(5, 1), gg, field)[1]
-
         out1 = gradient(gg, field)[1]
+        out2 = FiniteDifferences.grad(central_fdm(5, 1), gg, field)[1]
         @test out1 .+ cis(1) ≈ out2  .+ cis(1)
         AS, _ = AngularSpectrum(field, 100e-6, 633e-9, 100e-6)
-
         f_AS(x) = sum(abs2.(x .- AS(cis.(x))[1]))
-
         out3 = gradient(f_AS, field)[1]
-
         @test out3 ≈ out1
+        @test out2 ≈ out1
+
+
+        field = zeros(ComplexF64, (15, 15))
+        field[5:6, 3:8] .= 1
+        gg(x) = sum(abs2.(x .- angular_spectrum(cis.(x), [100e-6, 200e-6], 633e-9, 100e-6)[1]))
+        out1 = gradient(gg, field)[1]
+        out2 = FiniteDifferences.grad(central_fdm(5, 1), gg, field)[1]
+        @test out1 .+ cis(1) ≈ out2  .+ cis(1)
+        AS, _ = AngularSpectrum(field, [100e-6, 200e-6], 633e-9, 100e-6)
+        f_AS(x) = sum(abs2.(x .- AS(cis.(x))[1]))
+        out3 = gradient(f_AS, field)[1]
+        @test out3 ≈ out1
+        @test out2 ≈ out1
 
     end
 
