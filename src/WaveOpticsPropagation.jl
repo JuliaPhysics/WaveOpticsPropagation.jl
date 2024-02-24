26c0c24ac66c6946aedf4334f70b7faaf2a2551f
roflmaostc <fxw+git@mailbox.org>
2024-02-24 14:56:55 +0100

    Adopt new API

--- src/WaveOpticsPropagation.jl
+++ src/WaveOpticsPropagation.jl
@@ -9,7 +9,22 @@ using FourierTools
 using IndexFunArrays
 using CUDA
 
+"""
+    Params{M, M2}
 
+Calls such as `AS = Angular_Spectrum(field, z, λ, L)` will store a `Params` object in `AS.params` that 
+contains the physical parameters of the field and the propagated field. 
+This is useful to keep track of the physical parameters of the field and the propagated field. 
+
+Has fields to store physical parameters
+- `y`: is the y-axis, so the first dimension of the field
+- `x`: is the x-axis, so the second dimension of the field
+- `yp`: is the y-axis of the propagated field.
+- `xp`: is the x-axis of the propagated field.
+- `L`: is the physical size of the initial field.
+- `Lp`: is the physical size of the propagated field.
+
+"""
 struct Params{M, M2}
     y::M
     x::M
--- test/scalable_angular_spectrum.jl
+++ test/scalable_angular_spectrum.jl
@@ -3,7 +3,7 @@
     @testset "Test gradient with Finite Differences" begin
         field = zeros(ComplexF64, (24, 24))
         field[14:16, 14:16] .= 1
-        ss, _  = ScalableAngularSpectrum(cis.(field), 100e-6, 633e-9, 100e-6) 
+        ss  = ScalableAngularSpectrum(cis.(field), 100e-6, 633e-9, 100e-6) 
         gg(x) = sum(abs2.(x .- ss(cis.(x))[1]))
         out2 = FiniteDifferences.grad(central_fdm(5, 1), gg, field)[1]
         out1 = gradient(gg, field)[1]
@@ -11,10 +11,10 @@
 
         field = zeros(ComplexF64, (19, 19))
         field[14:16, 14:16] .= 1
-        ss, _  = ScalableAngularSpectrum(cis.(field), 100e-6, 633e-9, 100e-6, skip_final_phase=false) 
-        gg(x) = sum(abs2.(x .- ss(cis.(x))[1]))
-        out2 = FiniteDifferences.grad(central_fdm(5, 1), gg, field)[1]
-        out1 = gradient(gg, field)[1]
+        ss  = ScalableAngularSpectrum(cis.(field), 100e-6, 633e-9, 100e-6, skip_final_phase=false) 
+        gg2(x) = sum(abs2.(x .- ss(cis.(x))[1]))
+        out2 = FiniteDifferences.grad(central_fdm(5, 1), gg2, field)[1]
+        out1 = gradient(gg2, field)[1]
         @test out1 .+ cis(1) ≈ out2  .+ cis(1)
 
     end
