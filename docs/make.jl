da30d4beedc6b11a6cbc2d27bc506c2e67c3ee21
abhro <5664668+abhro@users.noreply.github.com>
2025-05-11 15:27:51 -0400

    Update `makedocs()` and `deploydocs()` options

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,24 @@
-using WaveOpticsPropagation, Documenter 
+using WaveOpticsPropagation, Documenter
 
 DocMeta.setdocmeta!(WaveOpticsPropagation, :DocTestSetup, :(using WaveOpticsPropagation); recursive=true)
-makedocs(modules = [WaveOpticsPropagation], 
-         sitename = "WaveOpticsPropagation.jl", 
-         pages = Any[
-            "WaveOpticsPropagation.jl" => "index.md",
-            "Function Docstrings" =>  "functions.md"
-         ],
-         warnonly=true,
-        )
 
-deploydocs(repo = "github.com/JuliaPhysics/WaveOpticsPropagation.jl.git", devbranch="main")
+makedocs(;
+    modules = [WaveOpticsPropagation],
+    sitename = "WaveOpticsPropagation.jl",
+    pages = Any[
+        "WaveOpticsPropagation.jl" => "index.md",
+        "Function Docstrings" => "functions.md",
+    ],
+    format = Documenter.HTML(;
+        assets = [
+            "assets/favicon.ico",
+        ],
+    ),
+    warnonly = true,
+)
+
+deploydocs(
+    repo = "github.com/JuliaPhysics/WaveOpticsPropagation.jl.git",
+    devbranch = "main",
+    push_preview = true,
+)
